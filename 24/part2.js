0613be87e1eb72dadf83c8e566e078e51b53e2ca
PiMastah <pimastah@gmail.com>
2021-12-24 10:19:25 +0100

    Day 24

---
+++ 24/part2.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const { EOL } = require('os')
+
+const input = 
+  fs.readFileSync('input.txt', 'utf8')
+  .split(EOL)
+
+const instructions = input.reduce((i, line) => {
+  const parts = line.split(' ')
+  i.push([parts[0], ...parts.slice(1).map(x => +x == x ? +x : x)])
+  return i
+}, [])
+
+const batches = instructions.reduce((b, instruction) => {
+  if (instruction[0] == 'inp') b.push([])
+  b[b.length-1].push(instruction)
+  return b
+}, []).map(b => [b[5][2],b[15][2]])
+
+const s = []
+
+let n = '.'.repeat(14).split('')
+batches.forEach(([c, o], i) => {
+  if (c>0) s.push([o, i])
+  if (c<0) {
+    const [off, from] = s.pop()
+    const d = off + c
+    if (d > 0) {
+      n[from] = 1
+      n[i] = 1 + d
+    } else {
+      n[from] = 1-d
+      n[i] = 1
+    }
+  }
+})
+
+console.log(n.join(''))
